8b71a90016719af409468f0814b831da209ddfaa
Banu Kurniawan <banu.kurniawan@gmail.com>
2026-08-19 20:45:57 +0700

    Configure trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             SetLocale::class,
             SecureHeaders::class,
